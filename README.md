5a39731a3505700cbb8896be74b364041a9c6511
Hirokazu Egashira <sekaibyoudou0712@gmail.com>
2024-07-07 12:25:35 +0900

    プラクティス追加

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # webpack-practice
 WEBPACK備忘録用
 WEBPACK実践入門 第2版に基づき練習記録
+
+※webpack-dev-serverがバージョン上depricatedになり、起動しなかったためwebpack-cli(5.1.4)、webpack-dev-server(5.0.4)をlatestで変更して対応
---
+++ using-webpack-dev-server/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'js/bundle.js',
+    },
+    devServer: {
+        open: true,
+        port: 9000,
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+    },
+}
